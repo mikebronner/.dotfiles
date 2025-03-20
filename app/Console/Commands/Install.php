d38e1e9250e681e0ad787a9ebb4f87a61eed69f8
Mike Bronner <mike@genealabs.com>
2025-03-20 06:28:42 -0700

    style: 💄 Adding views and formatting text.

--- app/Console/Commands/Install.php
+++ app/Console/Commands/Install.php
@@ -8,6 +8,10 @@ use App\Actions\InstallGit;
 use Illuminate\Console\Command;
 use Illuminate\Console\View\Components\Task;
 
+use function Laravel\Prompts\confirm;
+use function Termwind\render;
+use function Termwind\terminal;
+
 class Install extends Command
 {
     protected $signature = "app:install";
@@ -15,13 +19,43 @@ class Install extends Command
 
     public function handle(): void
     {
+        $this->showIntro();
+
+        if (! $this->shouldContinue()) {
+            render(view("exit")->render());
+
+            exit;
+        }
+
+        $this->askQuestions();
         $this->installStep('Installing Git', function (): void {
             InstallGit::run();
         });
     }
 
+    protected function askQuestions(): void
+    {
+
+    }
+
     protected function installStep(string $message, callable $callback): void
     {
         (new Task($this->output))->render($message, $callback);
+    }
+
+    protected function shouldContinue(): bool
+    {
+        return confirm(
+            default: false,
+            label: "Would you like to continue?",
+            no: "Not right now.",
+            yes: "Yes",
+        );
+    }
+
+    protected function showIntro(): void
+    {
+        terminal()->clear();
+        render(view("welcome")->render());
     }
 }
